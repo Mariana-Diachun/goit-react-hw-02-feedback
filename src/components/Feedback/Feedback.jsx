14b9ce46f190b22d798c4ff207e2e56990ff5c0b
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-10-14 12:14:08 +0200

    markup done

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,82 @@
+import 'components/Feedback/Feedback.styled';
+import { Component } from 'react';
+import { Notification } from 'components/Notification/Notification';
+
+export class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleIncrementGood = () => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+  };
+
+  handleIncrementNeutral = () => {
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
+  handleIncrementBad = () => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const values = Object.values(this.state);
+    let total = 0;
+    for (const value of values) {
+      total += value;
+    }
+    return total;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const values = Object.values(this.state);
+    let total = 0;
+    for (const value of values) {
+      total += value;
+    }
+    const good = Math.floor((this.state.good * 100) / Number(total));
+    return good;
+  };
+
+  render() {
+    return (
+      <section>
+        <h1>Please leave feedback</h1>
+        <div>
+          <button type="button" onClick={this.handleIncrementGood}>
+            Good
+          </button>
+          <button type="button" onClick={this.handleIncrementNeutral}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.handleIncrementBad}>
+            Bad
+          </button>
+        </div>
+        <h2>Statistics</h2>
+        {this.countTotalFeedback() === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <div>
+            <p>Good:{this.state.good}</p>
+            <p>Neutral:{this.state.neutral}</p>
+            <p>Bad:{this.state.bad}</p>
+            <p>Total:{this.countTotalFeedback()}</p>
+            <p>Positive feedback:{this.countPositiveFeedbackPercentage()}%</p>
+          </div>
+        )}
+      </section>
+    );
+  }
+}
